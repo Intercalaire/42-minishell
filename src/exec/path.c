4d2bd21626572803a250d46ff6789836337d4569
Henry Solet <hsolet@1H3.42angouleme.fr>
2024-05-01 15:27:34 +0200

    Henry ajout recup path

---
+++ src/exec/path.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   path.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsolet <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 14:18:09 by hsolet            #+#    #+#             */
+/*   Updated: 2024/05/01 15:05:36 by hsolet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include "../../include/minishell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//dans les cas ou c'est different de echo, cd, pwd, export, unset, env, exit
+void path(void)
+{
+	char *path;
+	char **var;
+	int bin;
+
+	path = calloc(strlen(getenv("PATH")), sizeof(char *));
+	path = getenv("PATH");
+	printf("%s", path);
+	var = ft_split(path, ":");
+	while (var[i])
+	{
+		if (ft_strncmp(var[i], "/bin", 5) == 0)
+			break ;
+		i++;
+	}
+	if (!var[i])
+		error("No /bin/");
+	else
+		bin = i;
+	execve(var[bin], argv, var);
+	//argv = commande + arguments
+
+}
+
+int main(void)
+{
+	path();
+	return 0;
+}
