861813b1ef0fb0ec55721f6046e8697e03b244a7
Henry Solet <hsolet@1G2.42angouleme.fr>
2024-05-14 11:47:47 +0200

    ajout debut gestion pipe

--- src/exec/pipe.c
+++ src/exec/pipe.c
@@ -6,8 +6,39 @@
 /*   By: hsolet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 13:10:52 by hsolet            #+#    #+#             */
-/*   Updated: 2024/05/04 13:10:54 by hsolet           ###   ########.fr       */
+/*   Updated: 2024/05/14 11:47:18 by hsolet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int pipe()
+{
+	int i;
+	int *son_pid;
 
+	i = 0;
+	while (i < nbr_pipe)
+	{
+		son_pid[i] = fork();
+		if (son_pid[i] == -1)
+			erreur();
+		i++;
+	}
+	i = 0;
+	while (son_pid[i] != getpid())
+		i++;
+	/*
+	 - ecrire chaque resultat dans un fichier que je vide apres lecture et que je reecris en boucle et detruit a la fin
+	 - ecrire dans une chaine de charactere les resultats et envoyer dans le fd demander que au niveau du pere
+	 -
+	 
+	 */
+	///////////////////////////////////////////////////////////
+	open(/*input*/);
+	open(/*output*/);
+
+	close(/*output*/);
+	close(/*input*/);
+	///////////////////////////////////////////////////////////
+	if (son_pid[i] == 0)
+		return(/*jsp encore mais c'est la fin*/);
+}
