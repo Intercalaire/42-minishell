287fcb6db70a936b779b958dbbae8624e2fd8d80
Henry Solet <hsolet@1G2.42angouleme.fr>
2024-05-15 15:16:40 +0200

    Add fct env

--- src/exec/pwd.c
+++ src/exec/pwd.c
@@ -6,13 +6,13 @@
 /*   By: hsolet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 11:19:50 by hsolet            #+#    #+#             */
-/*   Updated: 2024/05/15 11:50:58 by hsolet           ###   ########.fr       */
+/*   Updated: 2024/05/15 12:28:21 by hsolet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+/*
 void pwd(void)
 {
 	int i;
@@ -28,33 +28,47 @@ void pwd(void)
 	}
 	printf("%s", str);
 }
-
-int pwd(t_data *data)
+*/
+int pwd(char **env)
 {
 	int i;
 
-	while (data->env[i])
+	i = 0;
+	while (env[i])
 	{
-		if (ft_strncmp(env_cpy[i], "PWD=", 4) && !ft_strncmp(env_cpy[i], "OLDPWD=", 7))
-			ft_printf("%s", ft_substr(data->env[i]))
+		if (strncmp(env[i], "PWD=", 4) && !strncmp(env[i], "OLDPWD=", 7))
+		{	
+			printf("%s", substr(env[i], 4, strlen(env[i])));
+			return(0);
+		}
 		i++;
 	}
+	return(2);
 }
 
-void env(t_data *data, char **environnement)
+char **env(char **environnement)
 {
+	char **env;
 	int len_env;
 	int i;
 
 	len_env = 0;
 	while (environnement[len_env])
 		len_env++;
-	data->env = ft_calloc(len_env, sizeof(*char));
-	if (!data->env)
+	env = calloc(len_env, sizeof(*char));
+	if (!env)
 		exit("Alloc Error\n");
 	while (environnement[i])
 	{
-		env[i] = ft_strdup(environnement[len_env])
+		env[i] = strdup(environnement[len_env])
 		i++;
 	}
+	return(env);
+}
+int main(int argc, char **argv, char **env)
+{
+	void(argc);
+	void(argv);
+	pwd(env(env));
+
 }
